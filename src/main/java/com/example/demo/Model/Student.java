1ee4cb9db603026f06959af95547e89ea6ec9a4a
csamiselo <csamiselo@github.com>
2019-12-16 16:31:49 +0200

    Implementation of the model class for the project

---
+++ src/main/java/com/example/demo/Model/Student.java
@@ -0,0 +1,41 @@
+package com.example.demo.Model;
+
+import java.util.UUID;
+
+/*
+* Model class implementation, The aim of the project is to implement the project using N-tier architecture
+* to enforce The separation of concerns technique
+*
+*
+*
+* */
+
+public class Student {
+    private final UUID id;
+    private final String first_name;
+    private  final String last_name;
+    private final String course;
+
+    public Student(UUID id, String first_name, String last_name, String course) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.course = course;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+}
